dea823bf07ced629b9c32e6fac3ce8cae66c61d0
Cory Edwards <cjedwards@machinetran.com>
2018-07-13 15:02:21 -0700

    Trying again
    Trying to commit project again.

---
+++ temp/Nail Salon Client Signup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nail Salon Client Signup
+//
+//  Created by Cory Edwards on 7/13/18.
+//  Copyright © 2018 Machine Translation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
