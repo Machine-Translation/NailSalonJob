6b0ff13d5dba90a2e02579a44a987ddce90734c1
Cory Edwards <cjedwards@machinetran.com>
2018-07-20 18:07:12 -0700

    More finalizing of UI

--- Salon Management/Salon Management/ClientTableViewCell.swift
+++ Salon Management/Salon Management/ClientTableViewCell.swift
@@ -8,7 +8,18 @@
 
 import UIKit
 
-class ClientTableViewCell: UITableViewCell {
+class ClientTableViewCell: UITableViewCell, UICollectionViewDataSource {
+    
+    
+    //MARK: Properties
+    @IBOutlet weak var clientNameLabel: UILabel!
+    @IBOutlet weak var signInTimeLabel: UILabel!
+    @IBOutlet weak var employeeNameLabel: UILabel!
+    @IBOutlet weak var timeBackLabel: UILabel!
+    @IBOutlet weak var clientNumberLabel: UILabel!
+    
+    var items = [String]()
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -19,6 +30,30 @@ class ClientTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    //MARK: Extra
+    func updateView() {
+        //Needs to force the collection view to update with new items.
+    }
+    
+    //MARK: UICollectionViewDataSource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath) as? ItemCollectionViewCell else {
+            fatalError("dequeued cell is not of ItemCollectionViewCell.")
+        }
+        
+        let item = items[indexPath.row]
+        
+        // Configure the cell
+        cell.itemNameLabel.text = item
+        
+        return cell
     }
 
 }
---
+++ Salon Management/Salon Management/ItemCollectionView.swift
@@ -0,0 +1,29 @@
+//
+//  ItemCollectionView.swift
+//  Salon Management
+//
+//  Created by Cory Edwards on 7/20/18.
+//  Copyright © 2018 Machine Translation. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionView: UICollectionView, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
